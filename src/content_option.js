4d8a9ec5caf84f12ea3b6b2104466f4d5f497277
William Alfano <billybr00dal@yahoo.com>
2024-09-01 21:38:40 -0400

    Added working EmailJS functionality. Added cursor: pointer on portfolio objects.

--- src/content_option.js
+++ src/content_option.js
@@ -55,15 +55,15 @@ const dataportfolio = [
 ]
 
 const contactConfig = {
-  YOUR_EMAIL: 'name@domain.com',
+  YOUR_EMAIL: 'wearehalfnhalf@gmail.com',
   YOUR_FONE: '(555)123-4567',
   description:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ',
   // creat an emailjs.com account
   // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-  YOUR_SERVICE_ID: 'service_id',
-  YOUR_TEMPLATE_ID: 'template_id',
-  YOUR_USER_ID: 'user_id'
+  YOUR_SERVICE_ID: 'service_0min8rf',
+  YOUR_TEMPLATE_ID: 'template_8r32rvq',
+  YOUR_USER_ID: 'GeY_1gtq4ZpoMrH3_'
 }
 
 const socialprofils = {
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -40,7 +40,7 @@ export const ContactUs = () => {
           console.log(result.text)
           setFormdata({
             loading: false,
-            alertmessage: 'SUCCESS! ,Thankyou for your messege',
+            alertmessage: 'Thanks for your message. We will be in touch soon!',
             variant: 'success',
             show: true
           })
@@ -48,7 +48,7 @@ export const ContactUs = () => {
         (error) => {
           console.log(error.text)
           setFormdata({
-            alertmessage: `Faild to send!,${error.text}`,
+            alertmessage: `Faild to send, ${error.text}`,
             variant: 'danger',
             show: true
           })
