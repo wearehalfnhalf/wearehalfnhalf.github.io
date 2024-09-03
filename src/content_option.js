bfa88804e5b7f648c412be4ae011609e89d0452b
half&half <wearehalfnhalf@gmail.com>
2024-09-03 00:00:59 -0400

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -56,9 +56,9 @@ const dataportfolio = [
 
 const contactConfig = {
   YOUR_EMAIL: 'wearehalfnhalf@gmail.com',
-  YOUR_FONE: '(908)123-4567',
+  YOUR_FONE: '(908)321-4567',
   description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra.',
   // creat an emailjs.com account
   // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
   YOUR_SERVICE_ID: 'service_0min8rf',
